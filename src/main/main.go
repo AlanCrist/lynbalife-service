d1eca6d6df218a7e9a6ef3b495f15e491ee9a967
Alan <lanzn16@outlook.com>
2020-02-01 21:57:01 -0300

    add tokn jwt for cookie

--- src/main/main.go
+++ src/main/main.go
@@ -45,6 +45,7 @@ func main() {
 	jwtGroup.Use(middleware.JWTWithConfig(middleware.JWTConfig{
 		SigningMethod: "HS512",
 		SigningKey:    []byte("mySecret"),
+		TokenLookup:   "cookie:JWTCookie",
 	}))
 
 	cookieGroup.Use(checkCookie)
@@ -100,6 +101,14 @@ func login(c echo.Context) error {
 			log.Println("Error Creating JWT token", err)
 			return c.String(http.StatusInternalServerError, "something went wrong")
 		}
+
+		jwtCookie := &http.Cookie{}
+
+		jwtCookie.Name = "JWTCookie"
+		jwtCookie.Value = token
+		jwtCookie.Expires = time.Now().Add(48 * time.Hour)
+
+		c.SetCookie(jwtCookie)
 
 		return c.JSON(http.StatusOK, map[string]string{
 			"message": "You were logged in!",
